64e59a42a4091bf0ecf48a10e7dfa8a3f95f33ca
Mike Zimmerman <corpiousleviosis@gmail.com>
2020-03-09 18:23:36 -0600

    Added miss table.
    Did some optimizing.
    Cleaned up some code to read more easily.

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from mlxtend.data import loadlocal_mnist
 from MnistNumber import MnistNumber
 import time
 
+MAX_READ_NUMBERS = 500
+
 def main_task():
     # Grab the mnist training data
     mnist_training_img_data, mnist_training_lbl_data = loadlocal_mnist(images_path="./Training_Data/train-images.idx3-ubyte", labels_path="./Training_data/train-labels.idx1-ubyte")
@@ -9,19 +11,20 @@ def main_task():
     mnist_testing_img_data, mnist_testing_lbl_data = loadlocal_mnist(images_path="./Testing_Data/t10k-images.idx3-ubyte", labels_path="./Testing_Data/t10k-labels.idx1-ubyte")
 
     # Average all the training data by numbers
-    training_data = []
-    for num in range(10):
-        training_data.append(MnistNumber(num))
+    training_data = [MnistNumber(i) for i in range(10)]
 
-    for num in range(len(mnist_training_img_data)):
-        for obj in training_data:
+    # MAX_READ_NUMBERS = len(mnist_training_img_data)
+
+    for obj in training_data:
+        for num in range(MAX_READ_NUMBERS):
             if obj.img_lbl == mnist_training_lbl_data[num]:
                 obj.add_img(mnist_training_img_data[num])
 
     # Go through each number in the testing data, and try them against the training data
+    num_to_check = len(mnist_testing_img_data)
     testing_data = []
     misses = []
-    num_to_check = len(mnist_testing_img_data)
+
     start = time.time()
     for num in range(num_to_check):
         # Create a MnistNumber using the current testing number
@@ -36,8 +39,16 @@ def main_task():
         if (testing_data[num].guess != testing_data[num].img_lbl):
             misses.append(testing_data[num])
     stop = time.time()
+
     # Print the Error rate
-    print(f"Total checked={num_to_check}; Missed={len(misses)}; Error Rate={len(misses)/num_to_check*100}%; Time each check={(stop-start)/num_to_check} s")
+    print(f"Total checked={num_to_check}; Missed={len(misses)}; Error Rate={round(len(misses)/num_to_check*100, 2)}%; Total time={round(stop-start, 2)}s; Time each check={round((stop-start)/num_to_check*1000,2)}ms")
+    
+    # Print miss table
+    grouped_misses = [[0 for i in range(10)] for j in range(10)]
+    for num in misses:
+        grouped_misses[num.img_lbl][num.guess] += 1
+    for num in grouped_misses:
+        print(num)
 
     # Uncomment the following two lines if you want to see what the misses looked like
     # for obj in misses:
--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Final project for machine learning.
 This project will be able to take handwritten digits and determine what number they represent.
 
+# Python Version
+Python 3.7.2
+
 # Required Modules
 - mlxtend
 - numpy
