51295317553491d8525e452f21986c789ba7bee1
Mike Zimmerman <corpiousleviosis@gmail.com>
2020-04-09 14:54:18 -0600

    Changes to make this working

--- MnistNumber.py
+++ MnistNumber.py
@@ -1,12 +1,16 @@
 from matplotlib import pyplot as plt
 import pathlib
 import numpy as np
+import math
 
 class MnistNumber:
     img_data = np.zeros([28,28])
+    pixel_count = 0
     img_lbl = 0
     num_img = 0
     guess = -1
+    pixelGuess = -1
+    threshhold = 80
     
     # Initializes using the given number and image array
     def __init__(self, number, img = None):
@@ -14,16 +18,27 @@ class MnistNumber:
         if img != None:
             self.add_img(img)
         else:
-            self.img_data = np.zeros([28,28])
+            self.img_data = np.zeros([28,28])   
 
     # Average the given image with the current average
     def add_img(self, new_data):
         self.num_img += 1
-        self.img_data += ((np.reshape(new_data, (28,28)) / 256)- self.img_data) / self.num_img
+        self.img_data += (np.reshape(new_data, (28,28)) - self.img_data) / self.num_img
+        if (self.pixel_count == 0):
+            self.pixel_count = (np.reshape(new_data, (28,28)) > self.threshhold).sum()
+        else:
+            self.pixel_count = (self.pixel_count +  (np.reshape(new_data, (28,28)) > self.threshhold).sum()) / 2
+            
 
     # Get the difference in energy of this number with the given number
     def get_energy_diff(self, com_num):
         return np.linalg.norm(com_num.img_data - self.img_data)
+
+    def get_pixel_diff(self, com_num):
+        return (((com_num.pixel_count - self.pixel_count)**2)**(1/2))
+
+    def get_pixel_diff(self, com_num):
+        return (((com_num.pixel_count - self.pixel_count)**2)**(1/2))
 
     # Check to see if this number is the same as the given number
     def check_num(self, num):
