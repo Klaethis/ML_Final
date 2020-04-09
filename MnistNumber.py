cadd338039be8f76d29ad3b587d093dcbb47afa8
Mike Zimmerman <corpiousleviosis@gmail.com>
2020-04-09 11:36:08 -0600

    Changed image data to be normalied.
    __init__ now uses add_img to ingest image data.
    add_img now divides every element in the image data by 256 to normalize.

--- MnistNumber.py
+++ MnistNumber.py
@@ -12,20 +12,18 @@ class MnistNumber:
     def __init__(self, number, img = None):
         self.img_lbl = number
         if img != None:
-            self.img_data = np.reshape(img, (28,28))
-            self.num_img = 1
+            self.add_img(img)
         else:
             self.img_data = np.zeros([28,28])
-            self.num_img = 0
 
     # Average the given image with the current average
     def add_img(self, new_data):
         self.num_img += 1
-        self.img_data += (np.reshape(new_data, (28,28)) - self.img_data) / self.num_img
+        self.img_data += ((np.reshape(new_data, (28,28)) / 256)- self.img_data) / self.num_img
 
     # Get the difference in energy of this number with the given number
     def get_energy_diff(self, com_num):
-        return ((com_num.img_data - self.img_data)**2).sum()
+        return np.linalg.norm(com_num.img_data - self.img_data)
 
     # Check to see if this number is the same as the given number
     def check_num(self, num):
