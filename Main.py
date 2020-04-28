a6fb6144576e9b3ffbc648b7482502ee3129b11d
Mike Zimmerman <corpiousleviosis@gmail.com>
2020-04-28 14:34:12 -0600

    Fixed voter algorithm. Added Transition algorithm.

--- Main.py
+++ Main.py
@@ -36,12 +36,13 @@ def main_task():
     X = np.hstack((onesLeft, X))
     Y = np.identity(10) #identity matrix of 10 for numbers 0-9 and identity to show the classification
     Bhat, residuals, rank, s = np.linalg.lstsq(np.transpose(X).dot(X), np.transpose(X).dot(Y), rcond=None)
-    #Yhat = X.dot(Bhat)
     
     # Error tables
-    MA_error = [0.1041,0.0379,0.2432,0.1941,0.1741,0.3139,0.1367,0.1673,0.2628,0.1933]
-    PA_error = [0.3561,0.4952,0.8924,0.8515,0.9104,0.8722,0.8904,0.9056,0.9928,0.9584]
-    LMS_error = [0.2898,0.1137,0.3537,0.2218,0.2525,0.3475,0.2276,0.2714,0.2916,0.2270]
+    MA_error = 1 - np.array([0.1041,0.0379,0.2432,0.1941,0.1741,0.3139,0.1367,0.1673,0.2628,0.1933])
+    PA_error = 1 - np.array([0.3561,0.4952,0.8924,0.8515,0.9104,0.8722,0.8904,0.9056,0.9928,0.9584])
+    LMS_error = 1 - np.array([0.2898,0.1137,0.3537,0.2218,0.2525,0.3475,0.2276,0.2714,0.2916,0.2270])
+    H_Trans_error = 1 - np.array([0.0673,0.9982,0.9738,0.9257,1.0000,1.0000,0.9937,1.0000,0.9220,1.0000])
+    V_Trans_error = 1 - np.array([0.8939,0.1656,0.8324,0.2683,0.7159,1.0000,1.0000,0.8696,1.0000,1.0000])
 
     # Go through each number in the testing data, and try them against the training data
     num_to_check = len(mnist_testing_img_data)
@@ -52,6 +53,10 @@ def main_task():
     hitsPixel = []
     missesLMS = []
     hitsLMS = []
+    missesHTransition = []
+    hitsHTransition = []
+    missesVTransition = []
+    hitsVTransition = []
     missesVoter = []
     hitsVoter = []
 
@@ -65,36 +70,42 @@ def main_task():
         testing_data[num].guess = 0
         testing_data[num].pixelGuess = 0
         testing_data[num].lmsGuess = 0
-        maxY = 0
-        lmsGuess = 0
-        counter = 0
+        testing_data[num].hTransitionGuess = 0
+        testing_data[num].vTransitionGuess = 0
+        testing_data[num].voterGuess = 0
+        
         for obj in training_data:
             if (obj.get_energy_diff(testing_data[num]) < training_data[testing_data[num].guess].get_energy_diff(testing_data[num])):
                 testing_data[num].guess = obj.img_lbl
             if (obj.get_pixel_diff(testing_data[num]) < training_data[testing_data[num].pixelGuess].get_pixel_diff(testing_data[num])):
                 testing_data[num].pixelGuess = obj.img_lbl
+            if (obj.get_h_transition_diff(testing_data[num]) < training_data[testing_data[num].hTransitionGuess].get_h_transition_diff(testing_data[num])):
+                testing_data[num].hTransitionGuess = obj.img_lbl
+            if (obj.get_v_transition_diff(testing_data[num]) < training_data[testing_data[num].vTransitionGuess].get_v_transition_diff(testing_data[num])):
+                testing_data[num].vTransitionGuess = obj.img_lbl
 
-        testing_data[num].lms_data = testing_data[num].img_data.flatten()
-        testRow = testing_data[num].lms_data
-        testRow1 = [1]
-        testRow = np.hstack((testRow1, testRow))
+        testRow = np.hstack(([1], testing_data[num].img_data.flatten()))
         yHat = testRow.dot(Bhat)
 
-        for yVal in yHat:
-            if (yVal > maxY):
-                 maxY = yVal #get the largest value in the yArray so that we know the guess
-                 lmsGuess = counter # this is the spot in the array (the actual guess)
-            counter = counter + 1
-        testing_data[num].lmsGuess = lmsGuess   
+        testing_data[num].lmsGuess = np.argmax(yHat) 
 
-        guess_index = np.argmin(np.array([MA_error[testing_data[num].guess],PA_error[testing_data[num].pixelGuess],LMS_error[testing_data[num].lmsGuess]]))
-
-        if (guess_index == 0):
-            testing_data[num].voterGuess = testing_data[num].guess
-        elif (guess_index == 1):
-            testing_data[num].voterGuess = testing_data[num].pixelGuess
-        else:
-            testing_data[num].voterGuess = testing_data[num].lmsGuess
+        last_confidence = 0
+        testing_data[num].voterGuess = 0
+        for count_num in range(10):
+            cur_confidence = 0
+            if (testing_data[num].guess == count_num):
+                cur_confidence += MA_error[testing_data[num].guess]
+            if (testing_data[num].pixelGuess == count_num):
+                cur_confidence += PA_error[testing_data[num].pixelGuess]
+            if (testing_data[num].lmsGuess == count_num):
+                cur_confidence += LMS_error[testing_data[num].lmsGuess]
+            if (testing_data[num].hTransitionGuess == count_num):
+                cur_confidence += H_Trans_error[testing_data[num].hTransitionGuess]
+            if (testing_data[num].vTransitionGuess == count_num):
+                cur_confidence += V_Trans_error[testing_data[num].vTransitionGuess]
+            if (cur_confidence > last_confidence):
+                testing_data[num].voterGuess = count_num
+                last_confidence = cur_confidence
 
         if (testing_data[num].guess != testing_data[num].img_lbl):
             misses.append(testing_data[num])
@@ -110,6 +121,16 @@ def main_task():
             missesLMS.append(testing_data[num])
         else:
             hitsLMS.append(testing_data[num])
+
+        if (testing_data[num].hTransitionGuess != testing_data[num].img_lbl):
+            missesHTransition.append(testing_data[num])
+        else:
+            hitsHTransition.append(testing_data[num])
+
+        if (testing_data[num].vTransitionGuess != testing_data[num].img_lbl):
+            missesVTransition.append(testing_data[num])
+        else:
+            hitsVTransition.append(testing_data[num])
 
         if (testing_data[num].voterGuess != testing_data[num].img_lbl):
             missesVoter.append(testing_data[num])
@@ -154,7 +175,6 @@ def main_task():
         print(f"{num}: Hits: {grouped_hitsPixel[num]}\tMisses:{np.array(grouped_missesPixel[num]).sum()}\tTotal: {(grouped_hitsPixel[num]+np.array(grouped_missesPixel[num]).sum())}\tError:{np.round(np.array(grouped_missesPixel[num]).sum()/(grouped_hitsPixel[num]+np.array(grouped_missesPixel[num]).sum()),4)*100}%")
 
 
-
     # Do it for LMS
     print(f"\nLinear Regression Method")
     print(f"Total checked={num_to_check}; Missed={len(missesLMS)}; Error Rate={round(len(missesLMS)/num_to_check*100, 2)}%; Total time={round(stop-start, 2)}s; Time each check={round((stop-start)/num_to_check*1000,2)}ms")
@@ -174,12 +194,49 @@ def main_task():
         print(f"{num}: Hits: {grouped_hitsLMS[num]}\tMisses:{np.array(grouped_missesLMS[num]).sum()}\tTotal: {(grouped_hitsLMS[num]+np.array(grouped_missesLMS[num]).sum())}\tError:{np.round(np.array(grouped_missesLMS[num]).sum()/(grouped_hitsLMS[num]+np.array(grouped_missesLMS[num]).sum()),4)*100}%")
 
 
-    # Do it for Voter
+    # Do it for LMS
+    print(f"\nHorizontal Transition Method")
+    print(f"Total checked={num_to_check}; Missed={len(missesHTransition)}; Error Rate={round(len(missesHTransition)/num_to_check*100, 2)}%; Total time={round(stop-start, 2)}s; Time each check={round((stop-start)/num_to_check*1000,2)}ms")
+
+
+    # Print miss table for horizontal Transition
+    grouped_missesHTransition = [[0 for i in range(10)] for j in range(10)]
+    for num in missesHTransition:
+        grouped_missesHTransition[num.img_lbl][num.hTransitionGuess] += 1
+    for num in grouped_missesHTransition:
+        print(f"{num} : {np.array(num).sum()}")
+
+    grouped_hitsHTransition = [0 for i in range(10)]
+    for num in hitsHTransition:
+        grouped_hitsHTransition[num.img_lbl] += 1
+    for num in range(len(grouped_hitsHTransition)):
+        print(f"{num}: Hits: {grouped_hitsHTransition[num]}\tMisses:{np.array(grouped_missesHTransition[num]).sum()}\tTotal: {(grouped_hitsHTransition[num]+np.array(grouped_missesHTransition[num]).sum())}\tError:{np.round(np.array(grouped_missesHTransition[num]).sum()/(grouped_hitsHTransition[num]+np.array(grouped_missesHTransition[num]).sum()),4)*100}%")
+
+
+    # Do it for LMS
+    print(f"\nVertical Transition Method")
+    print(f"Total checked={num_to_check}; Missed={len(missesVTransition)}; Error Rate={round(len(missesVTransition)/num_to_check*100, 2)}%; Total time={round(stop-start, 2)}s; Time each check={round((stop-start)/num_to_check*1000,2)}ms")
+
+    # Print miss table for Vertical Transition
+    grouped_missesVSTransition = [[0 for i in range(10)] for j in range(10)]
+    for num in missesVTransition:
+        grouped_missesVSTransition[num.img_lbl][num.vTransitionGuess] += 1
+    for num in grouped_missesVSTransition:
+        print(f"{num} : {np.array(num).sum()}")
+
+    grouped_hitsVSTransition = [0 for i in range(10)]
+    for num in hitsVTransition:
+        grouped_hitsVSTransition[num.img_lbl] += 1
+    for num in range(len(grouped_hitsVSTransition)):
+        print(f"{num}: Hits: {grouped_hitsVSTransition[num]}\tMisses:{np.array(grouped_missesVSTransition[num]).sum()}\tTotal: {(grouped_hitsVSTransition[num]+np.array(grouped_missesVSTransition[num]).sum())}\tError:{np.round(np.array(grouped_missesVSTransition[num]).sum()/(grouped_hitsVSTransition[num]+np.array(grouped_missesVSTransition[num]).sum()),4)*100}%")
+
+
+    # Do it for horizontal Transition
     print(f"\nVoter Method")
     print(f"Total checked={num_to_check}; Missed={len(missesVoter)}; Error Rate={round(len(missesVoter)/num_to_check*100, 2)}%; Total time={round(stop-start, 2)}s; Time each check={round((stop-start)/num_to_check*1000,2)}ms")
 
 
-    # Print miss table for LMS
+    # Print miss table for Voter
     grouped_missesVoter = [[0 for i in range(10)] for j in range(10)]
     for num in missesVoter:
         grouped_missesVoter[num.img_lbl][num.voterGuess] += 1
--- MnistNumber.py
+++ MnistNumber.py
@@ -5,15 +5,21 @@ import math
 
 class MnistNumber:
     img_data = np.zeros([28,28])
-    pixel_count = 0
+    bw_img_data = np.zeros([28,28]) > 0
     img_lbl = 0
     num_img = 0
+
     guess = -1
     pixelGuess = -1
     lmsGuess = -1
+    hTransitionGuess = -1
+    vTransitionGuess = -1
     voterGuess = -1
+
     threshhold = 80
-    lms_data = np.zeros([0,28**2])
+    pixel_count = 0
+    h_trans_count = 0
+    v_trans_count = 0
     
     # Initializes using the given number and image array
     def __init__(self, number, img = None):
@@ -27,11 +33,27 @@ class MnistNumber:
     def add_img(self, new_data):
         self.num_img += 1
         self.img_data += (np.reshape(new_data, (28,28)) - self.img_data) / self.num_img
-        if (self.pixel_count == 0):
-            self.pixel_count = (np.reshape(new_data, (28,28)) > self.threshhold).sum()
-        else:
-            self.pixel_count = (self.pixel_count +  (np.reshape(new_data, (28,28)) > self.threshhold).sum()) / 2
-            
+
+        self.bw_img_data = (self.img_data > self.threshhold)
+        self.pixel_count = self.bw_img_data.sum()
+
+        h_flat = self.bw_img_data.flatten(order="C")
+        v_flat = self.bw_img_data.flatten(order="F")
+        
+        last_v = h_flat[0]
+        last_h = v_flat[0]
+
+        self.h_trans_count = 0
+        self.v_trans_count = 0
+        
+        for num in range(len(h_flat)):
+            if (last_h != h_flat[num]):
+                self.h_trans_count += 1
+            if (last_v != v_flat[num]):
+                self.v_trans_count += 1
+            last_h = h_flat[num]
+            last_v = v_flat[num]
+
 
     # Get the difference in energy of this number with the given number
     def get_energy_diff(self, com_num):
@@ -39,6 +61,12 @@ class MnistNumber:
 
     def get_pixel_diff(self, com_num):
         return (((com_num.pixel_count - self.pixel_count)**2)**(1/2))
+
+    def get_h_transition_diff(self, com_num):
+        return np.linalg.norm(com_num.h_trans_count - self.h_trans_count)
+
+    def get_v_transition_diff(self, com_num):
+        return np.linalg.norm(com_num.v_trans_count - self.v_trans_count)
 
     # Check to see if this number is the same as the given number
     def check_num(self, num):
@@ -66,6 +94,3 @@ class MnistNumber:
         plt.figure(self.img_lbl)
         plt.plot(list(range(0, 28**2)), self.lms_data)
         plt.show()
-
-    #def getOutputY(self):
-
